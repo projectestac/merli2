e1c089f97c2e7804f46d92f57b13b9014610542d
Joan Sala <jsala236@xtec.cat>
2019-05-14 18:13:39 +0200

    Updates the localization constants interface

--- manager/src/main/java/cat/xtec/merli/duc/client/messages/DucConstants.java
+++ manager/src/main/java/cat/xtec/merli/duc/client/messages/DucConstants.java
@@ -51,49 +51,72 @@ public interface DucConstants extends ConstantsWithLookup {
     @DefaultStringValue("Choose an option")
     String CHOOSE_AN_OPTION();
 
-    /* Types for 'EntityType' */
-
-    @DefaultStringValue("Root concept")
-    String ROOT_CONCEPT();
-
-    /* Types for 'CategoryType' */
-
-    @DefaultStringValue("Education phase")
-    String EDUCATION_PHASE();
-
-    @DefaultStringValue("Education stage")
-    String EDUCATION_STAGE();
-
-    @DefaultStringValue("Academic year")
-    String ACADEMIC_YEAR();
-
-    @DefaultStringValue("Field of study")
-    String FIELD_OF_STUDY();
-
-    @DefaultStringValue("Programme of study")
-    String PROGRAMME_OF_STUDY();
-
-    @DefaultStringValue("Topic of study")
-    String TOPIC_OF_STUDY();
-
-    /* Types for 'context' */
-
-    @DefaultStringValue("Higher education")
-    String HIGHER_EDUCATION();
-
-    @DefaultStringValue("Other")
-    String OTHER();
-
-    @DefaultStringValue("School")
-    String SCHOOL();
-
-    @DefaultStringValue("Training")
-    String TRAINING();
-
-    /* Types for 'contributorRole' */
+    /* Normal user of a learning object (LOMv1.0) */
 
     @DefaultStringValue("Author")
     String AUTHOR();
+
+    @DefaultStringValue("Learner")
+    String LEARNER();
+
+    @DefaultStringValue("Manager")
+    String MANAGER();
+
+    @DefaultStringValue("Teacher")
+    String TEACHER();
+
+    /* Content knowledge type (DUCv3.0) */
+
+    @DefaultStringValue("Unspecified")
+    String UNSPECIFIED();
+
+    @DefaultStringValue("Attitudinal")
+    String ATTITUDINAL();
+
+    @DefaultStringValue("Conceptual")
+    String CONCEPTUAL();
+
+    @DefaultStringValue("Procedural")
+    String PROCEDURAL();
+
+    /* Whether copyright or other restrictions apply (LOMv1.0) */
+
+    @DefaultStringValue("Has restrictions")
+    String HAS_RESTRICTIONS();
+
+    @DefaultStringValue("Without restrictions")
+    String WITHOUT_RESTRICTIONS();
+
+    /* Content licenses (DUCv3.0) */
+
+    @DefaultStringValue("Creative Commons: Attribution (CC BY)")
+    String CREATIVE_COMMONS_BY();
+
+    @DefaultStringValue("Creative Commons: Attribution-ShareAlike (CC BY-SA)")
+    String CREATIVE_COMMONS_BYSA();
+
+    @DefaultStringValue("Creative Commons: Attribution-NoDerivatives (CC BY-ND)")
+    String CREATIVE_COMMONS_BYND();
+
+    @DefaultStringValue("Creative Commons: Attribution-NonCommercial (CC BY-NC)")
+    String CREATIVE_COMMONS_BYNC();
+
+    @DefaultStringValue("Creative Commons: Attribution-NonCommercial-ShareAlike (CC BY-NC-SA)")
+    String CREATIVE_COMMONS_BYNCSA();
+
+    @DefaultStringValue("Creative Commons: Attribution-NonCommercial-NoDerivatives (CC BY-NC-ND)")
+    String CREATIVE_COMMONS_BYNCND();
+
+    @DefaultStringValue("Edu3 educational license")
+    String EDU3_EDUCATIONAL();
+
+    @DefaultStringValue("Other copyright licenses")
+    String OTHER_COPYRIGHT();
+
+    @DefaultStringValue("Public domain")
+    String PUBLIC_DOMAIN();
+
+    /* Lifecycle contributor roles (LOMv1.0) */
 
     @DefaultStringValue("Content provider")
     String CONTENT_PROVIDER();
@@ -140,154 +163,7 @@ public interface DucConstants extends ConstantsWithLookup {
     @DefaultStringValue("Validator")
     String VALIDATOR();
 
-    /* Types for 'Cost' */
-
-    @DefaultStringValue("Requires payment")
-    String REQUIRES_PAYMENT();
-
-    @DefaultStringValue("Free of charge")
-    String WITHOUT_CHARGE();
-
-    /* Types for 'Copyright' */
-
-    @DefaultStringValue("Creative Commons: Attribution (CC BY)")
-    String CC_BY();
-
-    @DefaultStringValue("Creative Commons: Attribution-ShareAlike (CC BY-SA)")
-    String CC_BY_SA();
-
-    @DefaultStringValue("Creative Commons: Attribution-NoDerivatives (CC BY-ND)")
-    String CC_BY_ND();
-
-    @DefaultStringValue("Creative Commons: Attribution-NonCommercial (CC BY-NC)")
-    String CC_BY_NC();
-
-    @DefaultStringValue("Creative Commons: Attribution-NonCommercial-ShareAlike (CC BY-NC-SA)")
-    String CC_BY_NC_SA();
-
-    @DefaultStringValue("Creative Commons: Attribution-NonCommercial-NoDerivatives (CC BY-NC-ND)")
-    String CC_BY_NC_ND();
-
-    @DefaultStringValue("Edu3")
-    String EDU3();
-
-    @DefaultStringValue("Other copyright licenses")
-    String COPYRIGHT();
-
-    @DefaultStringValue("Public domain")
-    String PUBLIC_DOMAIN();
-
-    /* Types for 'Knowledge' */
-
-    @DefaultStringValue("Unspecified")
-    String UNSPECIFIED();
-
-    @DefaultStringValue("Attitudinal")
-    String ATTITUDINAL();
-
-    @DefaultStringValue("Conceptual")
-    String CONCEPTUAL();
-
-    @DefaultStringValue("Procedural")
-    String PROCEDURAL();
-
-    /* Types for 'kind' */
-
-    @DefaultStringValue("Is part of")
-    String IS_PART_OF();
-
-    @DefaultStringValue("Has part")
-    String HAS_PART();
-
-    @DefaultStringValue("Is version of")
-    String IS_VERSION_OF();
-
-    @DefaultStringValue("Has version")
-    String HAS_VERSION();
-
-    @DefaultStringValue("Is format of")
-    String IS_FORMAT_OF();
-
-    @DefaultStringValue("Has format")
-    String HAS_FORMAT();
-
-    @DefaultStringValue("References")
-    String REFERENCES();
-
-    @DefaultStringValue("Is referenced by")
-    String IS_REFERENCED_BY();
-
-    @DefaultStringValue("Is based on")
-    String IS_BASED_ON();
-
-    @DefaultStringValue("Is basis for")
-    String IS_BASIS_FOR();
-
-    @DefaultStringValue("Requires")
-    String REQUIRES();
-
-    @DefaultStringValue("Is required by")
-    String IS_REQUIRED_BY();
-
-    /* Types for 'TermType' */
-
-    @DefaultStringValue("Facet indicator")
-    String FACET_INDICATOR();
-
-    @DefaultStringValue("Preferred term")
-    String PREFERRED_TERM();
-
-    @DefaultStringValue("Non-preferred term")
-    String NON_PREFERRED_TERM();
-
-    /* Types for 'learningResourceType' */
-
-    @DefaultStringValue("Exercise")
-    String EXERCISE();
-
-    @DefaultStringValue("Simulation")
-    String SIMULATION();
-
-    @DefaultStringValue("Questionnaire")
-    String QUESTIONNAIRE();
-
-    @DefaultStringValue("Diagram")
-    String DIAGRAM();
-
-    @DefaultStringValue("Figure")
-    String FIGURE();
-
-    @DefaultStringValue("Graph")
-    String GRAPH();
-
-    @DefaultStringValue("Index")
-    String INDEX();
-
-    @DefaultStringValue("Lecture")
-    String LECTURE();
-
-    @DefaultStringValue("Slide")
-    String SLIDE();
-
-    @DefaultStringValue("Table")
-    String TABLE();
-
-    @DefaultStringValue("Narrative text")
-    String NARRATIVE_TEXT();
-
-    @DefaultStringValue("Exam")
-    String EXAM();
-
-    @DefaultStringValue("Experiment")
-    String EXPERIMENT();
-
-    @DefaultStringValue("Problem statement")
-    String PROBLEM_STATEMENT();
-
-    @DefaultStringValue("Self assessment")
-    String SELF_ASSESMENT();
-
-    /* Types for 'status' */
+    /* The state or condition of a learning object (LOMv1.0) */
 
     @DefaultStringValue("Draft")
     String DRAFT();
@@ -301,7 +177,91 @@ public interface DucConstants extends ConstantsWithLookup {
     @DefaultStringValue("Unavailable")
     String UNAVAILABLE();
 
-    /* Types for 'structure' */
+    /* Physical medium of the resource (DUCv3.0) */
+
+    @DefaultStringValue("Audio")
+    String AUDIO();
+
+    @DefaultStringValue("Data")
+    String DATA();
+
+    @DefaultStringValue("Image")
+    String IMAGE();
+
+    @DefaultStringValue("Text")
+    String TEXT();
+
+    @DefaultStringValue("Video")
+    String VIDEO();
+
+    @DefaultStringValue("Application")
+    String APPLICATION();
+
+    @DefaultStringValue("Educational game")
+    String EDUCATIONAL_GAME();
+
+    @DefaultStringValue("Textbook")
+    String TEXTBOOK();
+
+    @DefaultStringValue("Presentation")
+    String PRESENTATION();
+
+    @DefaultStringValue("Social media")
+    String SOCIAL_MEDIA();
+
+    @DefaultStringValue("Brochure")
+    String BROCHURE();
+
+    @DefaultStringValue("Dossier")
+    String DOSSIER();
+
+    @DefaultStringValue("Equipment")
+    String EQUIPMENT();
+
+    @DefaultStringValue("Interactive textbook")
+    String INTERACTIVE_TEXTBOOK();
+
+    @DefaultStringValue("Magazine")
+    String MAGAZINE();
+
+    @DefaultStringValue("Map")
+    String MAP();
+
+    @DefaultStringValue("Musical audio")
+    String MUSICAL_AUDIO();
+
+    @DefaultStringValue("Photograph")
+    String PHOTOGRAPH();
+
+    @DefaultStringValue("Printable")
+    String PRINTABLE();
+
+    @DefaultStringValue("Text document")
+    String TEXT_DOCUMENT();
+
+    @DefaultStringValue("Visual media")
+    String VISUAL_MEDIA();
+
+    @DefaultStringValue("Requires payment")
+    String REQUIRES_PAYMENT();
+
+    @DefaultStringValue("Free of charge")
+    String WITHOUT_CHARGE();
+
+    @DefaultStringValue("International Standard Book Number (ISBN)")
+    String ISBN();
+
+    @DefaultStringValue("International Standard Serial Number (ISSN)")
+    String ISSN();
+
+    @DefaultStringValue("Legal deposit (NBN)")
+    String LEGAL_DEPOSIT();
+
+    @DefaultStringValue("Internal reference")
+    String MERLI();
+
+    @DefaultStringValue("Another catalog")
+    String OTHER_CATALOG();
 
     @DefaultStringValue("Atomic")
     String ATOMIC();
@@ -318,13 +278,107 @@ public interface DucConstants extends ConstantsWithLookup {
     @DefaultStringValue("Networked")
     String NETWORKED();
 
-    /* Types for 'termRelation' */
+    @DefaultStringValue("Compulsory education")
+    String COMPULSORY_EDUCATION();
 
-    @DefaultStringValue("Use")
-    String USE();
+    @DefaultStringValue("Continuing education")
+    String CONTINUING_EDUCATION();
 
-    @DefaultStringValue("Use for")
-    String USE_FOR();
+    @DefaultStringValue("Distance education")
+    String DISTANCE_EDUCATION();
+
+    @DefaultStringValue("Educational administration")
+    String EDUCATIONAL_ADMINISTRATION();
+
+    @DefaultStringValue("Higher education")
+    String HIGHER_EDUCATION();
+
+    @DefaultStringValue("Library")
+    String LIBRARY();
+
+    @DefaultStringValue("Policy making")
+    String POLICY_MAKING();
+
+    @DefaultStringValue("Pre-school")
+    String PRESCHOOL();
+
+    @DefaultStringValue("Professional development")
+    String PROFESSIONAL_DEVELOPMENT();
+
+    @DefaultStringValue("Special education")
+    String SPECIAL_EDUCATION();
+
+    @DefaultStringValue("Vocational education")
+    String VOCATIONAL_EDUCATION();
+
+    @DefaultStringValue("Other")
+    String OTHER();
+
+    @DefaultStringValue("Accessibility restrictions")
+    String ACCESSIBILITY_RESTRICTIONS();
+
+    @DefaultStringValue("Competency")
+    String COMPETENCY();
+
+    @DefaultStringValue("Discipline")
+    String DISCIPLINE();
+
+    @DefaultStringValue("Educational level")
+    String EDUCATIONAL_LEVEL();
+
+    @DefaultStringValue("Educational objective")
+    String EDUCATIONAL_OBJECTIVE();
+
+    @DefaultStringValue("Idea")
+    String IDEA();
+
+    @DefaultStringValue("Prerequisite")
+    String PREREQUISITE();
+
+    @DefaultStringValue("Security level")
+    String SECURITY_LEVEL();
+
+    @DefaultStringValue("Skill level")
+    String SKILL_LEVEL();
+
+    @DefaultStringValue("Educational context")
+    String EDUCATIONAL_CONTEXT();
+
+    @DefaultStringValue("Subject descriptor")
+    String SUBJECT_DESCRIPTOR();
+
+    @DefaultStringValue("Assessment")
+    String ASSESSMENT();
+
+    @DefaultStringValue("Drill and practice")
+    String DRILL_AND_PRACTICE();
+
+    @DefaultStringValue("Exploration")
+    String EXPLORATION();
+
+    @DefaultStringValue("Glossary")
+    String GLOSSARY();
+
+    @DefaultStringValue("Guide")
+    String GUIDE();
+
+    @DefaultStringValue("Open activity")
+    String OPEN_ACTIVITY();
+
+    @DefaultStringValue("Tool")
+    String TOOL();
+
+    @DefaultStringValue("Web page")
+    String WEB_PAGE();
+
+    @DefaultStringValue("Entity")
+    String ENTITY();
+
+    @DefaultStringValue("Term")
+    String TERM();
+
+    @DefaultStringValue("Resource")
+    String RESOURCE();
 
     @DefaultStringValue("Top term")
     String TOP_TERM();
@@ -338,32 +392,221 @@ public interface DucConstants extends ConstantsWithLookup {
     @DefaultStringValue("Related term")
     String RELATED_TERM();
 
-    /* Types for 'UserRole' */
+    @DefaultStringValue("Use")
+    String USE();
 
-    @DefaultStringValue("Learner")
-    String LEARNER();
+    @DefaultStringValue("Use for")
+    String USE_FOR();
 
-    @DefaultStringValue("Manager")
-    String MANAGER();
+    @DefaultStringValue("Requires")
+    String REQUIRES();
 
-    @DefaultStringValue("Teacher")
-    String TEACHER();
+    @DefaultStringValue("References")
+    String REFERENCES();
 
-    /* Identifier catalogs */
+    @DefaultStringValue("Has part")
+    String HAS_PART();
 
-    @DefaultStringValue("ISBN")
-    String ISBN();
+    @DefaultStringValue("Has version")
+    String HAS_VERSION();
 
-    @DefaultStringValue("ISSN")
-    String ISSN();
+    @DefaultStringValue("Has format")
+    String HAS_FORMAT();
 
-    @DefaultStringValue("Legal deposit")
-    String LEGAL_DEPOSIT();
+    @DefaultStringValue("Is required by")
+    String IS_REQUIRED_BY();
 
-    @DefaultStringValue("Another catalog")
-    String OTHER_CATALOG();
+    @DefaultStringValue("Is referenced by")
+    String IS_REFERENCED_BY();
 
-    /* Language names */
+    @DefaultStringValue("Is part of")
+    String IS_PART_OF();
+
+    @DefaultStringValue("Is version of")
+    String IS_VERSION_OF();
+
+    @DefaultStringValue("Is format of")
+    String IS_FORMAT_OF();
+
+    @DefaultStringValue("Is based on")
+    String IS_BASED_ON();
+
+    @DefaultStringValue("Is basis for")
+    String IS_BASIS_FOR();
+
+    /* Entity types (DUCv3.0) */
+
+    @DefaultStringValue("Root concept")
+    String ROOT_CONCEPT();
+
+    @DefaultStringValue("Contenct category")
+    String CONTENT_CATEGORY();
+
+    @DefaultStringValue("Vocabulary term")
+    String VOCABULARY_TERM();
+
+    @DefaultStringValue("Free term")
+    String FREE_TERM();
+
+    @DefaultStringValue("Learning object")
+    String LEARNING_OBJECT();
+
+    @DefaultStringValue("Education phase")
+    String EDUCATION_PHASE();
+
+    @DefaultStringValue("Education stage")
+    String EDUCATION_STAGE();
+
+    @DefaultStringValue("Academic year")
+    String ACADEMIC_YEAR();
+
+    @DefaultStringValue("Field of study")
+    String FIELD_OF_STUDY();
+
+    @DefaultStringValue("Programme of study")
+    String PROGRAMME_OF_STUDY();
+
+    @DefaultStringValue("Topic of study")
+    String TOPIC_OF_STUDY();
+
+    @DefaultStringValue("Facet indicator")
+    String FACET_INDICATOR();
+
+    @DefaultStringValue("Preferred term")
+    String PREFERRED_TERM();
+
+    @DefaultStringValue("Non-preferred term")
+    String NON_PREFERRED_TERM();
+
+    /* Technical data type of the learing object (LREv3.0) */
+
+    @DefaultStringValue("Application/base64")
+    String APPLICATION_BASE64();
+
+    @DefaultStringValue("Binary Software")
+    String APPLICATION_BINARY();
+
+    @DefaultStringValue("Java Application")
+    String APPLICATION_JAVA();
+
+    @DefaultStringValue("Application/macbinhex40")
+    String APPLICATION_MACBINHEX40();
+
+    @DefaultStringValue("Microsoft Excel")
+    String APPLICATION_MSEXCEL();
+
+    @DefaultStringValue("Microsoft Word")
+    String APPLICATION_MSWORD();
+
+    @DefaultStringValue("Ogg")
+    String APPLICATION_OGG();
+
+    @DefaultStringValue("Adobe Portable Document Format (PDF)")
+    String APPLICATION_PDF();
+
+    @DefaultStringValue("PostScript")
+    String APPLICATION_POSTSCRIPT();
+
+    @DefaultStringValue("Application/ppt")
+    String APPLICATION_PPT();
+
+    @DefaultStringValue("Rich Text Format (RTF)")
+    String APPLICATION_RTF();
+
+    @DefaultStringValue("Unix-to-Unix encoding")
+    String APPLICATION_UUE();
+
+    @DefaultStringValue("Application/x-compressed")
+    String APPLICATION_COMPRESSED();
+
+    @DefaultStringValue("Application/x-gzip-compressed")
+    String APPLICATION_GZIP_COMPRESSED();
+
+    @DefaultStringValue("Application/x-pn-realmedia")
+    String APPLICATION_REALMEDIA();
+
+    @DefaultStringValue("Adobe Flash")
+    String APPLICATION_SHOCKWAVE_FLASH();
+
+    @DefaultStringValue("Stuffit Archive")
+    String APPLICATION_STUFFIT();
+
+    @DefaultStringValue("Application/x-zip-compressed")
+    String APPLICATION_ZIP_COMPRESSED();
+
+    @DefaultStringValue("Zip Archive")
+    String APPLICATION_ZIP();
+
+    @DefaultStringValue("Au file format")
+    String AUDIO_BASIC();
+
+    @DefaultStringValue("Musical Instrument Digital Interface (MIDI)")
+    String AUDIO_MIDI();
+
+    @DefaultStringValue("MPEG Audio Layer (MP3)")
+    String AUDIO_MP3();
+
+    @DefaultStringValue("MPEG Audio")
+    String AUDIO_MPEG();
+
+    @DefaultStringValue("Waveform Audio Format (WAV)")
+    String AUDIO_WAV();
+
+    @DefaultStringValue("Real Audio Sound")
+    String AUDIO_REALAUDIO();
+
+    @DefaultStringValue("Real Audio Sound Plugin")
+    String AUDIO_REALAUDIO_PLUGIN();
+
+    @DefaultStringValue("Bitmap Image File (BMP)")
+    String IMAGE_BMP();
+
+    @DefaultStringValue("Graphics Interchange Format (GIF)")
+    String IMAGE_GIF();
+
+    @DefaultStringValue("JPEG Image (JPG)")
+    String IMAGE_JPEG();
+
+    @DefaultStringValue("Portable Network Graphics (PNG)")
+    String IMAGE_PNG();
+
+    @DefaultStringValue("Tagged Image File Format (TIFF)")
+    String IMAGE_TIFF();
+
+    @DefaultStringValue("Windows Metafile (WMF)")
+    String IMAGE_WMF();
+
+    @DefaultStringValue("Virtual Reality Modeling Language (VRML)")
+    String MODEL_VRML();
+
+    @DefaultStringValue("HyperText Markup Language (HTML)")
+    String TEXT_HTML();
+
+    @DefaultStringValue("Text File")
+    String TEXT_PLAIN();
+
+    @DefaultStringValue("Rich Text Format (RTF)")
+    String TEXT_RICHTEXT();
+
+    @DefaultStringValue("Extensible Markup Language (XML)")
+    String TEXT_XML();
+
+    @DefaultStringValue("Audio Video Interleave (AVI)")
+    String VIDEO_AVI();
+
+    @DefaultStringValue("MPEG Video")
+    String VIDEO_MPEG();
+
+    @DefaultStringValue("Quicktime Video")
+    String VIDEO_QUICKTIME();
+
+    @DefaultStringValue("RealMedia Video")
+    String VIDEO_REALVIDEO();
+
+    @DefaultStringValue("RealMedia Video Plugin")
+    String VIDEO_REALVIDEO_PLUGIN();
+
+    /* ISO 639-1 languages (LREv3.0) */
 
     @DefaultStringValue("Afar")
     String AFAR();
